1ab5d78ceac6fbbd19165cab3a3ad77424ea1d14
EIngram <ingram.ericm@gmail.com>
2016-10-08 10:28:55 -0500

    Adds nest status, home/away mode

--- Nest.py
+++ Nest.py
@@ -6,10 +6,22 @@ from Credentials import User, Pass
 
 # User = ''  # Enter email address here or store in a config
 # Pass = ''  # Enter your password or store in a config
-
-
+status = None
 N = nest.Nest(User, Pass)
 
 
-for structure in N.structures:
-    structure.away = False
+def is_home():  # set status to Home
+    for structure in N.structures:
+        structure.away = False
+
+
+def go_away():  # set status to Away
+    for structure in N.structures:
+        structure.away = True
+
+
+def nest_status():  # get current status from nest. True = Away, False = Home
+    global status
+    for structure in N.structures:
+        status = structure.away
+        return status
